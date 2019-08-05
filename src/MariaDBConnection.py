aae3eb9d1f79a03199f45acb64f77a8b993ff7f3
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2019-08-05 17:38:22 +0600

    updated

--- src/MariaDBConnection.py
+++ src/MariaDBConnection.py
@@ -6,51 +6,51 @@ import mysql.connector as mariadb
 class MariaDbConn:
 
 	def __init__(self, appFolder: str, obj):
-        self.__connection = None
+		self.__connection = None
 		self.__mainObj = obj
-        with open(appFolder + 'DbConfig.json', 'r', encoding='utf-8') as file:
-            self.dbConfig: dict = json.load(file)
+		with open(appFolder + 'DbConfig.json', 'r', encoding='utf-8') as file:
+			self.dbConfig: dict = json.load(file)
 
-    def connect(self):
-        try:
-            self.__connection = mariadb.connect(
-                host=self.dbConfig.get('host'),
-                port=self.dbConfig.get('port'),
-                database=self.dbConfig.get('database'),
-                user=self.dbConfig.get('username'),
-                passwd=self.dbConfig.get('password')
-            )
-            print("Connection successfull")
+	def connect(self):
+		try:
+			self.__connection = mariadb.connect(
+				host=self.dbConfig.get('host'),
+				port=self.dbConfig.get('port'),
+				database=self.dbConfig.get('database'),
+				user=self.dbConfig.get('username'),
+				passwd=self.dbConfig.get('password')
+			)
+			print("Connection successfull")
 			self.__mainObj.showStatus("Connection successfull")
-        except Exception as e:
-            print(e)
-            try:
-                self.__connection = mariadb.connect(
-                    host=self.dbConfig.get('host'),
-                    port=self.dbConfig.get('port'),
-                    # database=self.dbConfig.get('database'),
-                    user=self.dbConfig.get('username'),
-                    passwd=self.dbConfig.get('password')
-                )
-                print("Connection successfull")
+		except Exception as e:
+			print(e)
+			try:
+				self.__connection = mariadb.connect(
+					host=self.dbConfig.get('host'),
+					port=self.dbConfig.get('port'),
+					# database=self.dbConfig.get('database'),
+					user=self.dbConfig.get('username'),
+					passwd=self.dbConfig.get('password')
+				)
+				print("Connection successfull")
 				self.__mainObj.showStatus("Connection successfull")
-            except Exception as e:
-                print(e)
+			except Exception as e:
+				print(e)
 				self.__mainObj.showStatus("Connection unsuccessfull")
-                print("Connection unsuccessfull")
+				print("Connection unsuccessfull")
 
-    def getConnection(self):
-        return self.__connection
+	def getConnection(self):
+		return self.__connection
 
-    def execute(self, query: str):
-        data = self.__connection.cursor()
-        data.execute(query)
-        return data.fetchall()
+	def execute(self, query: str):
+		data = self.__connection.cursor()
+		data.execute(query)
+		return data.fetchall()
 
-    def close(self):
-        try:
-            self.__connection.close()
-            print("Close operation is successfull")
-        except Exception as e:
-            print(e)
-            print("Close operation isn't successfull")
+	def close(self):
+		try:
+			self.__connection.close()
+			print("Close operation is successfull")
+		except Exception as e:
+			print(e)
+			print("Close operation isn't successfull")
