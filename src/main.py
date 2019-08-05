497810b5e1aa680ba4643a47f42e39889005023f
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2019-08-05 12:39:27 +0600

    updated

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ from PyQt5 import uic
 from PyQt5 import QtGui
 from PyQt5.QtGui import QIcon, QFont, QPixmap, QMovie
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QLabel, QPushButton, QErrorMessage, QStyleFactory, \
-	QTableWidget, QTableWidgetItem, QTableWidgetSelectionRange
+	QTableWidget, QTableWidgetItem, QTableWidgetSelectionRange, QTableView
 from PyQt5.QtWidgets import QFileDialog, QDesktopWidget, QTextEdit
 from PyQt5.QtCore import pyqtSlot, QSize, pyqtSignal, QThread
 
