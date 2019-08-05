dde0314ecf2b48a5340576981324ef452fb9dc69
Rizwan-Hasan <rizwan.hasan486@gmail.com>
2019-08-05 17:13:21 +0600

    updated

--- src/launcher.py
+++ src/launcher.py
@@ -1,12 +1,15 @@
 import os
 import sys
+import json
+import mysql.connector
 from re import compile as regexCompile
 
 # PyQt5 Imports
 from PyQt5 import uic
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QStyleFactory
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QStyleFactory, QTableWidgetItem
 from PyQt5.QtWidgets import QDesktopWidget
+from PyQt5.QtCore import Qt
 
 # noinspection PyUnresolvedReferences
 import resources
